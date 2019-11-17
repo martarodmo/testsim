e0e257da70ad91bfb79dc53db5443adb912b152d
Live session user <martarodmo@gmail.com>
2019-11-17 17:46:28 +0000

    Corrección de issues

--- scripts/analyse_sample.sh
+++ scripts/analyse_sample.sh
@@ -10,10 +10,6 @@ then
     mkdir -p $WD/out/fastqc
     fastqc -o $WD/out/fastqc data/${sampleid}*.fastq.gz
     echo
-    echo "Running STAR index..."
-    mkdir -p $WD/res/genome/star_index
-    STAR --runThreadN 4 --runMode genomeGenerate --genomeDir $WD/res/genome/star_index/ --genomeFastaFiles $WD/res/genome/ecoli.fasta --genomeSAindexNbases 9
-    echo
     echo "Running STAR alignment..."
     mkdir -p $WD/out/star/${sampleid}
     STAR --runThreadN 4 --genomeDir $WD/res/genome/star_index/ --readFilesIn $WD/out/cutadapt/${sampleid}_1.trimmed.fastq.gz $WD/out/cutadapt/${sampleid}_2.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix $WD/out/star/${sampleid}/
--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,6 +1,10 @@
 # Establecer directorio de trabajo: export WD=<ruta_del_directorio>
 # Comando de ejecución: bash scripts/run_pipeline.sh >>  log/run_pipeline.out 2>&1
 
+echo "###########################################"
+echo "# CLEARING Working Directory (`echo $WD`)..."
+echo "###########################################"
+
 rm -r log/cutadapt 2>/dev/null
 rm -r res/genome 2>/dev/null
 rm -r out 2>/dev/null
@@ -14,6 +18,15 @@ mkdir -p res/genome
 wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 gunzip -k res/genome/ecoli.fasta.gz
 rm res/genome/ecoli.fasta.gz
+
+
+echo "###########################################"
+echo "# GENERARING INDEX..."
+echo "###########################################"
+echo "Running STAR index..."
+mkdir -p $WD/res/genome/star_index
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir $WD/res/genome/star_index/ --genomeFastaFiles $WD/res/genome/ecoli.fasta --genomeSAindexNbases 9
+echo
 
 # place here any commands that need to be run before analysing the samples
 for sample in $(ls data/*.fastq.gz | cut -d "_" -f1 | sed 's:data/::' | sort | uniq);
@@ -30,7 +43,6 @@ echo "###########################################"
 mkdir -p out/multiqc
 multiqc -o out/multiqc $WD
 echo "###########################################"
-echo "# EXPORTING ENVIRONMENT..."
+echo "# END"
 echo "###########################################"
-mkdir -p envs
-conda env export > envs/rna-seq.yaml
+
